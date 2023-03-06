fb8f2b1a91a93fd1ed2b2203e690b0d9b5cd2b08
Sergey Perfilev <sergey.perfilev@faraway.gg>
2023-03-06 08:25:52 +0530

    Add event discriminator

--- crates/anchor-idl/src/event.rs
+++ crates/anchor-idl/src/event.rs
@@ -18,6 +18,15 @@ pub fn generate_events(
                 let struct_name = format_ident!("{}", def.name);
                 let opts = struct_opts.get(&def.name).copied().unwrap_or_default();
 
+                let discriminator: proc_macro2::TokenStream = {
+                    let discriminator_preimage = format!("event:{}", struct_name);
+                    let mut discriminator = [0u8; 8];
+                    discriminator.copy_from_slice(
+                        &anchor_syn::hash::hash(discriminator_preimage.as_bytes()).to_bytes()[..8],
+                    );
+                    format!("{:?}", discriminator).parse().unwrap()
+                };
+
                 let fields = def
                     .fields
                     .iter()
@@ -27,7 +36,17 @@ pub fn generate_events(
                     })
                     .collect::<Vec<_>>();
 
-                generate_struct(&typedefs, &struct_name, &fields, opts)
+                let struct_ts = generate_struct(&typedefs, &struct_name, &fields, opts);
+
+                quote! {
+                    #struct_ts
+
+                    impl anchor_lang::Discriminator for #struct_name {
+                        fn discriminator() -> [u8; 8] {
+                            #discriminator
+                        }
+                    }
+                }
             });
             quote! {
                 #(#defined)*
