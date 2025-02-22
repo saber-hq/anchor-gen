9cd808877f99d6a8da464062f7b827de492d94ff
Emanuele Cesena <emanuele.cesena@gmail.com>
2025-02-22 16:24:03 -0700

    jup: add gov and vote

--- glam.sh
+++ glam.sh
@@ -26,14 +26,38 @@ $CPI_GEN $DRIFT_IDL \
     > $DRIFT_OUT
 
 #
+# JUPITER GOV
+#
+JUP_GOV_IDL=$(realpath ../glam/anchor/deps/jupiter_gov/govern.json)
+JUP_GOV_OUT=../glam/anchor/programs/glam/src/cpi_autogen/jupiter_gov.rs
+JUP_VOTE_IDL=$(realpath ../glam/anchor/deps/jupiter_vote/locked_voter.json)
+JUP_VOTE_OUT=../glam/anchor/programs/glam/src/cpi_autogen/jupiter_vote.rs
+
+# toggleMaxLock excluded because we do custom authz
+
+$CPI_GEN $JUP_VOTE_IDL --idl-name-alias jupiter_vote \
+    --ixs newEscrow \
+    --ixs increaseLockedAmount \
+    --ixs openPartialUnstaking \
+    --ixs mergePartialUnstaking \
+    --ixs withdraw \
+    --ixs withdrawPartialUnstaking \
+    --ixs castVote \
+    > $JUP_VOTE_OUT
+
+$CPI_GEN $JUP_GOV_IDL --idl-name-alias jupiter_gov \
+    --ixs newVote \
+    > $JUP_GOV_OUT
+
+#
 # KAMINO
 #
 KAMINO_IDL=$(realpath ../glam/anchor/deps/kamino_lending/kamino_lending.json)
 KAMINO_OUT=../glam/anchor/programs/glam/src/cpi_autogen/kamino_lending.rs
 
-$CPI_GEN $KAMINO_IDL \
-    --ixs initUserMetadata \
-    --ixs initObligation \
-    --ixs initObligationFarmsForReserve \
-    --ixs depositReserveLiquidityAndObligationCollateral \
-    > $KAMINO_OUT
+# $CPI_GEN $KAMINO_IDL \
+#     --ixs initUserMetadata \
+#     --ixs initObligation \
+#     --ixs initObligationFarmsForReserve \
+#     --ixs depositReserveLiquidityAndObligationCollateral \
+#     > $KAMINO_OUT
